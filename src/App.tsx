445674130832db80c68521bc8f34545426516d22
euler.amorim <euler.amorim@hotmail.com>
2026-05-06 10:46:59 -0300

    add Heading component with styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import './styles/theme.css';
 import './styles/global.css';
+import { Heading } from './components/Heading';
 
 export function App() {
   return (
     <>
+      <Heading />
       <h1>Hello World (do app)</h1>
     </>
   );
