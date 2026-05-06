71c8156e57f3ea17cb7c2a258ff6a0a6fed71aec
euler.amorim <euler.amorim@hotmail.com>
2026-05-06 14:53:32 -0300

    Refactor App component to use Container and Heading components; add Container styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,23 @@
 import './styles/theme.css';
 import './styles/global.css';
+import { Container } from './components/Container';
+import { Heading } from './components/Heading';
 
 export function App() {
   return (
     <>
-      <div className='container'>
-        <div className='content'>
-          <section>Logo</section>
-        </div>
-      </div>
-      <div className='container'>
-        <div className='content'>
-          <section>Menu</section>
-        </div>
-      </div>
-      <div className='container'>
-        <div className='content'>
-          <section>Formulario</section>
-        </div>
-      </div>
-      <div className='container'>
-        <div className='content'>
-          <section>Footer</section>
-        </div>
-      </div>
+      <Container>
+        <Heading>Logo</Heading>
+      </Container>
+      <Container>
+        <Heading>Menu</Heading>
+      </Container>
+      <Container>
+        <Heading>Formulario</Heading>
+      </Container>
+      <Container>
+        <Heading>Footer</Heading>
+      </Container>
     </>
   );
 }
