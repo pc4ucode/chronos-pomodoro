92091b5112d9db967a982bcefb7f0ef7e94426a2
euler.amorim <euler.amorim@hotmail.com>
2026-05-08 15:07:03 -0300

    Add theme toggle functionality and update styles for light mode

--- src/components/Menu/index.tsx
+++ src/components/Menu/index.tsx
@@ -1,19 +1,54 @@
 import { HistoryIcon, HouseIcon, SettingsIcon, SunIcon } from 'lucide-react';
 import styles from './styles.module.css';
+import { useState } from 'react';
+
+type AvailableThemes = 'dark' | 'light';
 
 export function Menu() {
+  const [theme, setTheme] = useState<AvailableThemes>('dark');
+
+  function handleThemeChange(
+    event: React.MouseEvent<HTMLAnchorElement, MouseEvent>,
+  ): void {
+    event.preventDefault();
+    const newTheme = theme === 'dark' ? 'light' : 'dark';
+    setTheme(newTheme);
+    document.documentElement.setAttribute('data-theme', newTheme);
+  }
+
   return (
     <nav className={styles.menu}>
-      <a className={styles.menuLink} href='#'>
+      <a
+        className={styles.menuLink}
+        href='#'
+        aria-label='Ir para a Home'
+        title='Ir para a Home'
+      >
         <HouseIcon />
       </a>
-      <a className={styles.menuLink} href='#'>
+      <a
+        className={styles.menuLink}
+        href='#'
+        aria-label='Ver Histórico'
+        title='Ver Histórico'
+      >
         <HistoryIcon />
       </a>
-      <a className={styles.menuLink} href='#'>
+      <a
+        className={styles.menuLink}
+        href='#'
+        aria-label='Configurações'
+        title='Configurações'
+      >
         <SettingsIcon />
       </a>
-      <a className={styles.menuLink} href='#'>
+      <a
+        className={styles.menuLink}
+        href='#'
+        aria-label='Mudar tema'
+        title='Mudar tema'
+        onClick={handleThemeChange}
+      >
         <SunIcon />
       </a>
     </nav>
