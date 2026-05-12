19beab6cbd8d758dc00d0a386c7c38e1e7791e37
euler.amorim <euler.amorim@hotmail.com>
2026-05-12 16:41:26 -0300

    Refactor MainForm to use useRef for task name input and update task creation logic

--- src/components/MainForm/index.tsx
+++ src/components/MainForm/index.tsx
@@ -2,13 +2,47 @@ import { PlayCircleIcon } from 'lucide-react';
 import { Cycles } from '../Cycles';
 import { DefaultButton } from '../DefaultButton';
 import { DefaultInput } from '../DefaultInput';
-import { useState } from 'react';
+import { useRef } from 'react';
+import type { TaskModel } from '../../models/TaskModel';
+import { useTaskContext } from '../../contexts/TaskContext/useTaskContext';
 
 export function MainForm() {
-  const [taskName, setTaskName] = useState('');
+  const { setState } = useTaskContext();
+  const taskNameInput = useRef<HTMLInputElement>(null);
+
   function handleCreateNewTask(event: React.FormEvent<HTMLFormElement>) {
     event.preventDefault();
-    console.log('deu certo!', taskName);
+
+    if (taskNameInput.current === null) return;
+
+    const taskName = taskNameInput.current.value.trim();
+
+    if (!taskName) {
+      alert('Digite o nome da tarefa');
+      return;
+    }
+
+    const newTask: TaskModel = {
+      id: Date.now().toString(),
+      name: taskName,
+      startDate: Date.now(),
+      completeDate: null,
+      interruptDate: null,
+      duration: 1,
+      type: 'workTime',
+    };
+
+    const secondsRemaining = newTask.duration * 60;
+
+    setState(prevState => ({
+      ...prevState,
+      config: { ...prevState.config },
+      activeTask: newTask,
+      currentCycle: 1,
+      secondsRemaining: secondsRemaining,
+      formattedSecondsRemaining: '00:00',
+      tasks: [...prevState.tasks, newTask],
+    }));
   }
 
   return (
@@ -19,8 +53,7 @@ export function MainForm() {
           type='text'
           id='task'
           placeholder='Digite algo'
-          value={taskName}
-          onChange={e => setTaskName(e.target.value)}
+          ref={taskNameInput}
         />
       </div>
 
