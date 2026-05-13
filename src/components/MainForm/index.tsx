9e78c7180ebc03b1467296e792286846130065e3
euler.amorim <euler.amorim@hotmail.com>
2026-05-13 12:06:38 -0300

    Add formatSecondsToMinutes utility and update task duration and seconds remaining formatting in MainForm

--- src/components/MainForm/index.tsx
+++ src/components/MainForm/index.tsx
@@ -7,6 +7,7 @@ import type { TaskModel } from '../../models/TaskModel';
 import { useTaskContext } from '../../contexts/TaskContext/useTaskContext';
 import { getNextCycle } from '../../utils/getNextCycle';
 import { getNextCycleType } from '../../utils/getNextCycleType';
+import { formatSecondsToMinutes } from '../../utils/formatSecondsToMinutes';
 
 export function MainForm() {
   const { state, setState } = useTaskContext();
@@ -33,7 +34,7 @@ export function MainForm() {
       startDate: Date.now(),
       completeDate: null,
       interruptDate: null,
-      duration: 1,
+      duration: state.config[nextCycleType],
       type: nextCycleType,
     };
 
@@ -44,8 +45,8 @@ export function MainForm() {
       config: { ...prevState.config },
       activeTask: newTask,
       currentCycle: nextCycle,
-      secondsRemaining: secondsRemaining,
-      formattedSecondsRemaining: '00:00',
+      secondsRemaining,
+      formattedSecondsRemaining: formatSecondsToMinutes(secondsRemaining),
       tasks: [...prevState.tasks, newTask],
     }));
   }
