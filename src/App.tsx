78e9fb6d786b646c7fbd5edacf873aa7188aecfb
euler.amorim <euler.amorim@hotmail.com>
2026-05-18 12:29:55 -0300

    feat: add react-router for navigation and update components to use Link

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,24 @@
-import { Home } from './pages/Home';
-import { TaskContextProvider } from './contexts/TaskContext/TaskContextProvider';
-
 import './styles/theme.css';
 import './styles/global.css';
+
+import { Home } from './pages/Home';
+import { TaskContextProvider } from './contexts/TaskContext/TaskContextProvider';
 import { MessagesContainer } from './components/MessagesContainer';
+import { BrowserRouter, Routes, Route } from 'react-router';
+import { NotFound } from './pages/NotFound';
+import { AboutPomodoro } from './pages/AboutPomodoro';
 
 export function App() {
   return (
     <TaskContextProvider>
       <MessagesContainer>
-        <Home />
+        <BrowserRouter>
+          <Routes>
+            <Route path='/' element={<Home />}></Route>
+            <Route path='/about-pomodoro' element={<AboutPomodoro />}></Route>
+            <Route path='*' element={<NotFound />}></Route>
+          </Routes>
+        </BrowserRouter>
       </MessagesContainer>
     </TaskContextProvider>
   );
