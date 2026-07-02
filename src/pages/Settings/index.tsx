be59672925f1d408cd14955d1d0b2932ce4e23a5
euler.amorim <euler.amorim@hotmail.com>
2026-07-02 16:22:39 -0300

    feat: enhance settings form validation and input types for time configuration

--- src/pages/Settings/index.tsx
+++ src/pages/Settings/index.tsx
@@ -6,6 +6,7 @@ import { Heading } from '../../components/Heading';
 import { MainTemplate } from '../../templates/MainTemplate';
 import { useRef } from 'react';
 import { useTaskContext } from '../../contexts/TaskContext/useTaskContext';
+import { showMessage } from '../../adapters/showMessage';
 
 export function Setting() {
   const { state } = useTaskContext();
@@ -15,16 +16,42 @@ export function Setting() {
 
   function handleSaveSettings(e: React.FormEvent<HTMLFormElement>) {
     e.preventDefault();
+    showMessage.dismiss();
 
-    const workTime = workTimeInput.current?.value;
-    const shortBreakTime = shortBreakTimeInput.current?.value;
-    const longBreakTime = longBreakTimeInput.current?.value;
+    const formErrors = [];
 
-    console.log('Configurações salvas:', {
-      workTime,
-      shortBreakTime,
-      longBreakTime,
-    });
+    const workTime = Number(workTimeInput.current?.value);
+    const shortBreakTime = Number(shortBreakTimeInput.current?.value);
+    const longBreakTime = Number(longBreakTimeInput.current?.value);
+
+    if (isNaN(workTime) || isNaN(shortBreakTime) || isNaN(longBreakTime)) {
+      formErrors.push('Digite apenas números válidos para todos os campos');
+    }
+
+    if (workTime < 1 || workTime > 99) {
+      formErrors.push('O tempo de foco deve estar entre 1 e 99 minutos');
+    }
+
+    if (shortBreakTime < 1 || shortBreakTime > 30) {
+      formErrors.push(
+        'O tempo de descanso curto deve estar entre 1 e 30 minutos',
+      );
+    }
+
+    if (longBreakTime < 1 || longBreakTime > 60) {
+      formErrors.push(
+        'O tempo de descanso longo deve estar entre 1 e 60 minutos',
+      );
+    }
+
+    if (formErrors.length > 0) {
+      formErrors.forEach(error => {
+        showMessage.error(error);
+      });
+      return;
+    }
+
+    console.log('Salvar');
   }
   return (
     <MainTemplate>
@@ -43,6 +70,7 @@ export function Setting() {
               labelText='Foco'
               ref={workTimeInput}
               defaultValue={state.config.workTime}
+              type='number'
             />
           </div>
           <div className='formRow'>
@@ -51,6 +79,7 @@ export function Setting() {
               labelText='Descanso Curto'
               ref={shortBreakTimeInput}
               defaultValue={state.config.shortBreakTime}
+              type='number'
             />
           </div>
           <div className='formRow'>
@@ -59,6 +88,7 @@ export function Setting() {
               labelText='Descanso Longo'
               ref={longBreakTimeInput}
               defaultValue={state.config.longBreakTime}
+              type='number'
             />
           </div>
           <div className='formRow'>
